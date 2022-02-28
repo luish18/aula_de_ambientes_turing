18845c5f0d83f5ea16fad03bdc4718329b4de1dd
Luís Henrique de Almeida Fernandes <luish.fernandes@usp.br>
2022-02-28 13:53:12 -0300

    corrects _reset_field method

--- snake_ruim/envs/snake_ruim.py
+++ snake_ruim/envs/snake_ruim.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import gym
 from gym import spaces
 from snake_ruim.utils.support_funcs import point
@@ -27,6 +28,7 @@ class snake_env(gym.Env):
                                                                                 })
                                              })
 
+        self.step_count = 0
         #espaco de acoes discreto (cima, baixo, direita, esquerda)
         self.action_space = spaces.Discrete(4)
 
@@ -41,13 +43,16 @@ class snake_env(gym.Env):
 
     def _reset_field(self) -> None:
 
-        with np.nditer(self.field, flags=["multi_index"]) as it:
+        it = np.nditer(self.field, flags=["multi_index"])
 
-            for cell in it:
+        for _ in it:
+            # montando paredes
+            if (it.multi_index[0] == 0) or (it.multi_index[1] == 0) or (it.multi_index[0] == (self.field_size - 1)) or (it.multi_index[1] == (self.field_size - 1)):
+                self.field[it.multi_index] = 1
+            else:
+                self.field[it.multi_index] = 1
 
-                # montando paredes
-                if (it.multi_index[0] == 0) or (it.multi_index[1] == 0) or (it.multi_index[0] == (self.field_size - 1)) or (it.multi_index[1] == (self.field_size - 1)):
-                    cell = 1
+
 
     def _generate_doces(self) -> None:
         """
@@ -71,7 +76,8 @@ class snake_env(gym.Env):
         #inicializa posicao dos doces
         self._generate_doces()
 
-        self.episode_reward = 0
+        self.episode_return = 0
+        self.step_count = 0
 
         obs = {
             "snake_position" : self.snake_pos(),
@@ -96,21 +102,69 @@ class snake_env(gym.Env):
 
     def _take_action(self, action):
 
-        if action = 0:
-            
+        if action == 0:
+            self.snake_pos.move("up")
+        elif action == 1:
+            self.snake_pos.move("down")
+        elif action == 2:
+            self.snake_pos.move("left")
+        elif action == 3:
+            self.snake_pos.move("right")
 
+    def _next_obs(self):
+
+        obs = {
+            "snake_position" : self.snake_pos(),
+            "candy_position" : self.doce_pos(),
+            "poison_position": self.veneno_pos(),
+            "neighbor_cells" : {
+                "up"         : self.field[self.snake_pos[0] - 1, self.snake_pos[1]],
+                "down"       : self.field[self.snake_pos[0] + 1, self.snake_pos[1]],
+                "left"       : self.field[self.snake_pos[0], self.snake_pos[1] - 1],
+                "right"      : self.field[self.snake_pos[0], self.snake_pos[1] + 1]
+            }
+        }
+
+        return obs
+
+    def _calculate_reward(self) -> Tuple[float, bool]:
+
+        reward = 0
+        done = False
+
+        if self.snake_pos == self.doce_pos:
+
+            reward += 10
+            self._generate_doces()
+
+        elif self.snake_pos == self.veneno_pos:
+
+            reward -= 100
+            done = True
+
+        elif (self.snake_pos[0] == 0) or (self.snake_pos[1] == 0) or (self.snake_pos[0] == self.field_size) or (self.snake_pos[1] == self.field_size):
+
+            reward -= 100
+            done = True
+
+        else:
+
+            reward += 1
+
+        return reward, done
 
 
     def step(self, action):
         """
         acoes validas: {cima:0, baixo:1, esquerda:2, direita:3}
         """
-        #TODO: write step function
 
-        _take_action(action)
-        next_obs()
-        reward()
-        done_logic()
+        #move o personagem na direcao de "action"
+        self._take_action(action)
+
+        #retorna dicionario com as observacoes atuais
+        obs = self._next_obs()
+        reward, done = self._calculate_reward()
 
 
         return obs, reward, done, {}
